2eaa6ed61474207c6632cc45bccb57ef200979d1
suresh1992 <suresh.palla.2010@gmail.com>
2019-08-18 21:06:30 +0530

    Website as complete 18th aug

--- kpglogistics/src/app/app.module.ts
+++ kpglogistics/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
+import { ClientCardsComponent } from './client-cards/client-cards.component';
+import { MissionVisionComponent } from './mission-vision/mission-vision.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeCarouselComponent,
+    ClientCardsComponent,
+    MissionVisionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
