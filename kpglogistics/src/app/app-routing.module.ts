7c5d7bbe235de7a93ecd6629d5b56ee3d4b90af5
suresh1992 <suresh.palla.2010@gmail.com>
2019-08-25 00:08:54 +0530

    Updated home carousel

--- kpglogistics/src/app/app-routing.module.ts
+++ kpglogistics/src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AppComponent } from './app.component';
+import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 
 const routes: Routes = [
+  { path: '', component: HomeCarouselComponent },
+  { path: 'home', component: HomeCarouselComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'contact-us', component: ContactUsComponent }
 ];
